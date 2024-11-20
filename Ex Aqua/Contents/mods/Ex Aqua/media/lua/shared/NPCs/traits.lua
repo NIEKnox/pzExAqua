b3aa43e75f315071b6de1799b930ed515773a65e
NIEKnox <noah.ie.knox@gmail.com>
2024-11-20 19:46:15 +0000

    Initial Commit

---
+++ Ex Aqua/Contents/mods/Ex Aqua/media/lua/shared/NPCs/traits.lua
@@ -0,0 +1,15 @@
+local addTrait = ProfessionFramework.addTrait
+
+addTrait("WaterWorldStart", {
+    name = "UI_trait_WaterWorldStart",
+    description = "UI_trait_WaterWorldStartDesc",
+    cost = 1,
+    inventory = {
+        ["Base.Hammer"] = 1,
+        ["Base.Screwdriver"] = 1,
+        ["Base.Saw"] = 1,
+        ["Base.SpearMachete"] = 1,
+        ["Base.FishingMag1"] = 1,
+        ["Base.NailsBox"] = 1,
+    },
+})
